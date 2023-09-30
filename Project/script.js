91a5fd7866cd33942cc9774ccf0b6dfa7064489b
adityadhara040505 <103095471+adityadhara040505@users.noreply.github.com>
2023-10-01 00:37:47 +0530

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,33 @@
+const apikey = "f349017823b2dcc53dba584a69f214dd";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiurl+city+`&appid=${apikey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+" celsius";
+    document.querySelector(".humidity").innerHTML = data.main.humidity+" %";
+    document.querySelector(".wind").innerHTML = data.wind.speed+" km/hr";
+}
+searchbtn.addEventListener("click",()=>{
+    checkWeather(searchbox.value);
+})
+const timel = document.getElementById("time");
+const datel = document.getElementById("date");
+const days = ["Sunday","Monday","Tuesday","Thursday","Friday","Saturday"];
+const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+setInterval(()=>{
+    const time = new Date();
+    const month =time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >=13 ? hour%12 : hour;
+    const minutes = time.getMinutes();
+    const ampm = hour>=12 ? "PM" : "AM";
+    timel.innerHTML = (hoursIn12HrFormat<10?0+hoursIn12HrFormat:hoursIn12HrFormat)+" : "+minutes+ampm;
+    datel = days[day]+", "+date+" "+months[month];
+}, 1000);
